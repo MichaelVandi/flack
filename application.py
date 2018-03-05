8dec456bd84bb6d1f2635ea911294372a39ead49
David J. Malan <malan@harvard.edu>
2018-03-04 23:51:51 -0500

    Update application.py

--- application.py
+++ application.py
@@ -1,9 +1,12 @@
+import os
+
 from flask import Flask
 from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
-app.config["SECRET_KEY"] = "secret!"
+app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 socketio = SocketIO(app)
+
 
 @app.route("/")
 def index():
