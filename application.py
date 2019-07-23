a81e221bc10bd4dfbe45ca1e3332d5854ea0883b
MichaelVandi <mikevee2013@gmail.com>
2019-07-23 04:53:01 -0400

    Satisfied with the way messages work between client and server

--- application.py
+++ application.py
@@ -1,8 +1,9 @@
 import os
 
-from flask import Flask, render_template, session, request
+from flask import Flask, render_template, jsonify, session, request
 from flask_session import Session
 from flask_socketio import SocketIO, emit
+from collections import defaultdict
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
@@ -12,52 +13,89 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
-# List to store usernames
+# List to store usernames, channels and messages
 names = []
 channels=[]
+# Storing messages as a dictionary with key = value of channel name
+messages = defaultdict(list)
 
-@app.route("/chat", methods=["GET", "POST"])
-def chat():
+@socketio.on("send text")
+def sendToEveryone(data):
+    text= str(data["text"])
+    date = str(data["date"])
+    name = str(data["name"])
+    complete_text = {'username': session["username"], 'content': text, 'date': date}
+    # Emit the new message to everyone
+    socketio.emit("new message", {"message": complete_text, "name": name}, include_self = False)
 
-    return render_template("chat.html", username= session["username"], channels=channels)
+@app.route("/text", methods=["POST"])
+def text():
+    text= str(request.form.get("text"))
+    date= str(request.form.get("date"))
+    index= int(request.form.get("id"))
+    name= str(request.form.get("name"))
+    complete_text = {'username': session["username"], 'content': text, 'date': date}
+    # Add text to the list of messages in that channel
+    messages[name].append(complete_text)
+    return jsonify({"messages": complete_text, "name": name})
+
+@socketio.on("open channel")
+def openChannel(data):
+    index= int(data["id"])
+    channelTexts = messages[data["name"]]
+    # Return channelTexts to DOM
+    emit("channel opened", {"messages": channelTexts}, broadcast=False)
+
+@socketio.on('flackbot')
+def flackbot(data):
+    # Create new mesage by Flackbot in the channel
+    content = "New channel created by " + session["username"]
+    # Text object is a dictionary with 3 keys, the username, the text content and the timestamp
+    text = {'username': 'Flackbot', 'content': content, 'date': data["date"]}
+    index =int(data["id"])
+    messages[data["name"]].append(text)
 
 @socketio.on("create channel")
-def createChannel(name):
+def createChannel(data):
     # Check if the channel list is empty and add fist channel
     if len(channels) == 0:
         # Channels are empty add new channel
-        channels.append(str(name))
+        channels.append(str(data["name"]))
         # Send the message
-        message="Channel Created Successfully"
-        emit("create feedback", {"message": message, "status": 1}, broadcast=True)
+        message="New Channel Created"
+        emit("create feedback", {"message": message, "name": str(data["name"]), "status": 1}, broadcast=True)
 
     # Else if the channel list is not empty, check if an existing channel is in list
     else:
-        if str(name) in channels:
+        if str(data["name"]) in channels:
             #Duplicate Channel, send error message
             message="Sorry there is another channel with the same name"
             emit("create feedback", {"message": message, "status": 0}, broadcast=True)
 
         # Otherwise, add the channel to the list of channels
         else:
-            channels.append(str(name))
+            channels.append(str(data["name"]))
             # Send the message
-            message="Channel Created Successfully"
-            emit("create feedback", {"message": message, "status": 1}, broadcast=True)
+            message="New Channel Created"
+            emit("create feedback", {"message": message, "name": str(data["name"]), "status": 1}, broadcast=True)
 
 
 @app.route("/" , methods=["GET", "POST"])
 def index():
     if session.get("username") is None:
-        session["username"] = ""
+         return render_template("index.html")
+    else:
+        return render_template("chat.html")
 
+@app.route("/chat", methods=["GET", "POST"])
+def chat():
     if request.method == "GET":
         # Getting the username variable stored in session
-        if session["username"] == "":
+        if session.get("username") is None:
             return render_template("index.html")
-
-        # If the user already entered a username, open the chat route
-        return render_template("chat.html", username=session["username"], channels=channels)
+        else:
+            # If the user already entered a username, open the chat route
+            return render_template("chat.html", username=session["username"], channels=channels)
 
     elif request.method == "POST":
         # Get the username sent from the post request
@@ -70,17 +108,18 @@ def index():
             session["username"] = username
             # Open the chat route
             return render_template("chat.html", username= username , channels=channels)
-
-        # If there were pre existing names
-        # Check if this particular username didn't exist
-        if username in names:
-            # Username was already there, return error
-            return render_template ("index.html", message= "Username already exists, pick something else")
         else:
-            # Username wasn't there, allow
-            names.append(username)
-            session["username"] = username
-            # Open the chat route
-            return render_template("chat.html", username=username, channels=channels)
+            # If there were pre existing names
+            # Check if this particular username didn't exist
+            if username in names:
+                # Username was already there, return error
+                session["username"] == None
+                return render_template ("index.html", message= "Username conflict, pick something else")
+            else:
+                # Username wasn't there, allow
+                names.append(username)
+                session["username"] = username
+                # Open the chat route
+                return render_template("chat.html", username=username, channels=channels)
 
    
--- static/index.js
+++ static/index.js
@@ -1,5 +1,7 @@
 document.addEventListener('DOMContentLoaded', ()=>{
 
+    var currentChannel= "none"
+    var currentChannelName;
     // Connect to websocket
     var socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port);
     var channelName_;
@@ -14,6 +16,7 @@ document.addEventListener('DOMContentLoaded', ()=>{
         else{
             // Send the data to flask server
             channelName_=channelName;
+            
             socket.emit('create channel', {'name': channelName});
         }
     }
@@ -21,21 +24,169 @@ document.addEventListener('DOMContentLoaded', ()=>{
     // Alert the feedback received from create channel broadcast
     socket.on('create feedback', data =>{
 
+        const li = document.createElement('li');
         switch(data.status){
+            // Status 1 for successful, 0 for not successful
             case 1:
-                const li = document.createElement('li');
-                li.innerHTML = `#${channelName_}`;
+                
+                // Giving channels an ID
+                li.setAttribute('class', 'channel');
+                li.innerHTML = `#${data.name}`;
+                // If everything was successful, add channel to list of channels
                 document.querySelector('#channels').append(li);
                 alert (data.message);
                 break;
             default:
                 alert (data.message);
-                break
+                break;
+        }
+        // Give all channels an id
+        $('#channels li').each(function(i,el){
+            el.id = 'chn'+ i;
+        });
+        // Add a Flack bot text to the channel
+        var currentDate = new Date(); 
+            var dateTime =  currentDate.getDate() + "/"
+                            + (currentDate.getMonth()+1)  + "/" 
+                            + currentDate.getFullYear() + " @ "  
+                            + currentDate.getHours() + ":"  
+                            + currentDate.getMinutes() + ":" 
+                            + currentDate.getSeconds();
+        // Get the current id of the newly created channel
+        var id = li.getAttribute('id').substring(3)
+        socket.emit('flackbot', {'id': id , 'date': dateTime, 'name': data.name})
+    })
 
+    // What happens when channels are clicked.
 
+    document.getElementById("channels").addEventListener("click", function (e){
+        // e is the targeted element or channel
+        // Checking if e is a list item
+        if(e.target && e.target.nodeName =="LI"){
+           var id = e.target.id;
+           // Removing the prefix from the id
+           var sub_id = id.substring(3)
+           var name = e.target.innerHTML;
+           // Setting that channels name to the heading
+           document.getElementById("heading").innerHTML=name.substring(1);
+           // Emit channel name to flask to create a list to hold messages
+           socket.emit('open channel', {'id': sub_id, 'name': name.substring(1)});
+           currentChannel = sub_id;
+           currentChannelName = name.substring(1);
+    
         }
     })
+
+    // Defining a function that creates new messages formated in a particular style when called
+    function createNewMessage (message){
+        var name_time_div = document.createElement('div');
+        name_time_div.setAttribute('class', 'name_time');
+
+        var name_paragraph = document.createElement('p');
+        name_paragraph.setAttribute('class', 'nameParagraph');
+        name_paragraph.innerHTML =`${message["username"]}`
+
+        var time_paragraph = document.createElement('p');
+        time_paragraph.setAttribute('class', 'timeParagraph')
+        time_paragraph.innerHTML =`${message["date"]}`
+
+        var content_paragraph =document.createElement('p');
+        content_paragraph.setAttribute('class', 'contentParagraph');
+        content_paragraph.innerHTML =`${message["content"]}`
+
+        //append views
+        name_time_div.appendChild(name_paragraph)
+        name_time_div.appendChild(time_paragraph)
+
+        var list = document.createElement('li');
+        list.appendChild(name_time_div)
+        list.appendChild(content_paragraph)
+        document.querySelector('#all_messages').append(list)
+    }
     
+    socket.on('channel opened', data =>{
+        var messages= data.messages;
+        // Refresh the list
+        document.querySelector('#all_messages').innerHTML =""
+        messages.forEach(function(element){
+            createNewMessage(element)
+        })
+        
+
+    })
+    
+    // What happens when text form is submitted
+    document.querySelector("#messageForm").onsubmit = () =>{
+        const request = new XMLHttpRequest();
+        var data;
+        request.open('POST', '/text');
+
+        // Callback goes here
+        request.onload = () => {
+
+            // Extract JSON data from request
+            const data = JSON.parse(request.responseText);
+            var messages = data.messages;
+            // Calling the function to create new message
+            createNewMessage(messages);
+            // Clear the input field
+            document.getElementById('textField').value=""
+            
+        }
+        // Callback goes here
+
+        // Check if no channel has been selected
+        if (currentChannel === "none"){
+           alert ("Please select a channel") 
+        }
+        
+        // User selected a channel
+        // Check if text message field is empty
+        const text_to_send= document.querySelector('#textField').value;
+        if(text_to_send ===""){
+            // Field is empty, return error
+            document.getElementById("errorField").innerHTML ="Text cannot be empty"
+            return false;
+        }
+        // Text field is not blank
+        // Set error text field to empty
+            
+        document.getElementById("errorField").innerHTML ="";
+        // Get the date and time
+        var currentDate = new Date(); 
+        var dateTime =  currentDate.getDate() + "/"
+                        + (currentDate.getMonth()+1)  + "/" 
+                        + currentDate.getFullYear() + " @ "  
+                        + currentDate.getHours() + ":"  
+                        + currentDate.getMinutes() + ":" 
+                        + currentDate.getSeconds();
+                        
+        var request_data = new FormData();
+        request_data.append('id', currentChannel)
+        request_data.append('text', text_to_send)
+        request_data.append('date', dateTime)
+        request_data.append('name', currentChannelName)
+        // Send request
+        request.send(request_data);
+        // Send to all clients
+        socket.emit('send text', {'text': text_to_send, 'date': dateTime, 'name':currentChannelName});
+        return false;
+    };
+
+    // What happens when a new message is broadcasted from flask server
+    socket.on('new message', data =>{
+        // Get the contents of them message
+        var message = data.message;
+        // Get the channel where the message was sent 
+        var messageChannel = data.name;
+        // Only load the message if that channel is currently opened
+        var openedChannel = document.getElementById('heading').innerHTML
+        if(openedChannel === messageChannel){
+            // Load the message here
+            // Call function to create new message
+            createNewMessage(message);
+        }
+    })
 
 
 
